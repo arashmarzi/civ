8bcfe1d738db077f7803b7161676fb01b670fe34
Arash Marzi <arash.marzi@gmail.com>
2015-05-02 16:28:14 -0400

    test resource & wood class

--- src/Game.java
+++ src/Game.java
@@ -3,6 +3,11 @@ import java.io.*;
 public class Game {
    public static void main(String[] args) {
       Wood wood = new Wood();
-      System.out.println(wood.getResType());
+      InfantrySquad is1 = new InfantrySquad();
+      System.out.println("res: " + wood.getResType());
+      while(wood.getSupply() != 0) {
+         System.out.println("extracted: " + wood.decrRes(is1.getWorkRate()));
+         System.out.println("supply: " + wood.getSupply());
+      }
    }
 }
--- src/Resource.java
+++ src/Resource.java
@@ -24,4 +24,8 @@ public class Resource {
          return extract;
       }
 
+      public int getSupply() {
+         return supply;
+      }
+
    }
